881d468e4b2e56216a7503c24b777b3cac566bd1
Durgaprasad0513 <durgaprasadvallabaneni@gmail.com>
2026-09-02 22:13:56 +0530

    chore: Clean up unneeded scratch scripts and temp files

--- fix_frontend_id.py
+++
@@ -1,9 +0,0 @@
-﻿import re
-
-with open("client/src/pages/assets/AssetListPage.tsx", "r", encoding="utf-8") as f:
-    content = f.read()
-
-content = content.replace("row.assignedEmployee?.id === user?.employeeId", "row.assignedEmployee?.id === (user?.employeeId || user?.employee?.id)")
-
-with open("client/src/pages/assets/AssetListPage.tsx", "w", encoding="utf-8") as f:
-    f.write(content)
--- server/check_db.js
+++
@@ -1,7 +0,0 @@
-﻿const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-async function main() {
-  const reqs = await prisma.requisition.findMany();
-  console.log("REQS:", reqs.length);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
